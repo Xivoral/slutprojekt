27104a2e02e80e4eb19895581769c75c3d2913e4
Xivor <tor.blom@elev.ga.lbs.se>
2026-05-07 13:07:35 +0200

    fungerandeskärm mycketssss väldigt nahjsbgs gaghahah

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
-
+#initaliserar pygame
 pygame.init()
+#speletsskärm
 screen = pygame.display.set_mode((800, 600))
+#färger
+GREEN = (0, 255, 0)
+BROWN = (150, 75, 0)
+RED = (255, 0, 0)
+#klockan
 clock = pygame.time.Clock()
+#spelsats
 game = True
-
+#spelloopen
 while game:
-    pygame.display.flip()
+    
+    screen.fill(GREEN)
+    pygame.display.update()
     clock.tick(60)
-
+    for event in pygame.event.get():      
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    
