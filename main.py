6056acbd302a587ff72c84a2946f5f1fba7fd752
Xivor <tor.blom@elev.ga.lbs.se>
2026-05-21 08:22:37 +0200

    jobbar vidare

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ pygame.display.set_caption("Fånga Äpplet")
 GREEN = (0, 255, 0)
 BROWN = (150, 75, 0)
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 #klockan
 clock = pygame.time.Clock()
 #spelsats
@@ -32,12 +33,26 @@ poäng = 0
 def måla_korg(x,y):
     pygame.draw.rect(screen, BROWN, [x,y,korg_bredd,korg_höjd])
 
-def måla_äpple(x,y):
+def måla_äpplen(x,y):
     pygame.draw.ellipse(screen, RED, [x,y,äpple_bredd,äpple_höjd])
-
+    
 def visa_poäng(poäng):
     font = pygame.font.SysFont(None, 36)
+    text = font.render("Poäng: " + str(poäng), True, WHITE)
+    screen.blit(text, (10,10))
+
+def spel_slut():
+    font = pygame.font.SysFont(None, 72)
+    text = font.render("Spel slut" + True, WHITE)
+    screen.blit(text, (skärm_bredd // 2 - 150, skärm_höjd // 2 - 36))
+    pygame.display.update()
+    pygame.time.delay(2000)
+    pygame.quit()
+    quit()
+
+def spel_loop():
     
+
 #spelloopen
 while game:
     
